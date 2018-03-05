00d952b9225cfec628d02cae5ce2f0b6eb58e7e6
Konrad Drozd <drozdkonrad@gmail.com>
2018-03-05 08:43:32 +0100

    Implement: Client is Server, Server is Client;

--- rsi_1/xmlrpc_client/src/main/java/RpcClient.java
+++ rsi_1/xmlrpc_client/src/main/java/RpcClient.java
@@ -1,36 +1,85 @@
 import org.apache.xmlrpc.AsyncCallback;
+import org.apache.xmlrpc.WebServer;
 import org.apache.xmlrpc.XmlRpcClient;
 import org.apache.xmlrpc.XmlRpcException;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class RpcClient {
 
-    public static final int PORT = 10001; //10000 + PC number
-    public static final String SERVER = "myServer";
+    public static final int PORT_AS_SERVER = 10001; //10000 + PC number
+    public static final String SERVER_NAME = "myServer2";
+    public static final String REMOTE_SERVER_NAME = "myServer1";
 
-    public static void main(String[] args) {
-        try {
-            XmlRpcClient client = new XmlRpcClient("http://localhost:" + PORT);
+    static private XmlRpcClient client;
+    static private WebServer webServer;
 
-            serverEcho(client);
+    public static void main(String[] args) throws MalformedURLException {
 
-            AsyncCallbackHandler acHandler = new AsyncCallbackHandler();
-            serverAsy(client, acHandler);
+        WebServer webServer = new WebServer(PORT_AS_SERVER);
+        webServer.addHandler(SERVER_NAME, new RpcClient());
+        webServer.start();
 
-            serverEcho(client);
+        client = new XmlRpcClient("http://localhost:" + 10002);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        System.out.println("Listening on port: " + PORT_AS_SERVER);
+        System.out.println("Sending to port: " + 10002);
+    }
+
+    public boolean cutshot() {
+        System.out.println("cutShot");
+        webServer.shutdown();
+        return true;
+    }
+
+    public boolean pingpong() {
+        sleep(1000);
+        if (new Random().nextInt(10) > 8) {
+            try {
+                cutShot(client);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("PONG");
+            try {
+                pingpong(client);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        return true;
+    }
 
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void pingpong(XmlRpcClient client) throws XmlRpcException, IOException {
+        System.out.println("PING");
+        Vector<Boolean> params = new Vector<Boolean>();
+        client.execute(REMOTE_SERVER_NAME + ".pingpong", params);
+    }
+
+    private static void cutShot(XmlRpcClient client) throws XmlRpcException, IOException {
+        System.out.println("SEND CUT_SHOT");
+        Vector<Boolean> params = new Vector<Boolean>();
+        client.execute(REMOTE_SERVER_NAME + ".cutshot", params);
     }
 
     private static void serverAsy(XmlRpcClient client, AsyncCallback acHandler) {
         Vector<Integer> params = new Vector<Integer>(2);
         params.add(1000);
-        client.executeAsync(SERVER + ".asy", params, acHandler);
+        client.executeAsync(SERVER_NAME + ".asy", params, acHandler);
         System.out.println("Async executed.");
     }
 
@@ -39,7 +88,7 @@ public class RpcClient {
         params.addElement(12);
         params.addElement(21);
 
-        Object result = client.execute(SERVER + ".echo", params);
+        Object result = client.execute(SERVER_NAME + ".echo", params);
 
         System.out.println(result);
         return result;
--- rsi_1/xmlrpc_server/src/main/java/RpcServer.java
+++ rsi_1/xmlrpc_server/src/main/java/RpcServer.java
@@ -1,17 +1,82 @@
 import org.apache.xmlrpc.WebServer;
+import org.apache.xmlrpc.XmlRpcClient;
+import org.apache.xmlrpc.XmlRpcException;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class RpcServer {
 
-    public static int PORT = 10001; //10000 + PC number
+    public static int PORT_AS_SERVER = 10002; //10000 + PC number
+    public static final String SERVER_NAME = "myServer1";
+    public static final String REMOTE_SERVER_NAME = "myServer2";
+    static XmlRpcClient client;
+    static WebServer webServer;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, XmlRpcException {
         System.out.println("Starting XML-RPC server");
 
-        WebServer webServer = new WebServer(PORT);
-        webServer.addHandler("myServer", new RpcServer());
+        WebServer webServer = new WebServer(PORT_AS_SERVER);
+        webServer.addHandler(SERVER_NAME, new RpcServer());
         webServer.start();
+        System.out.println("XML-RPC server started on port:" + PORT_AS_SERVER);
 
-        System.out.println("XML-RPC server started on port:" + PORT);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Server port: ");
+        int portAsAClient = in.nextInt();
+
+        client = new XmlRpcClient("http://localhost:" + portAsAClient);
+        pingpong(client);
+
+    }
+
+    public boolean cutshot() {
+        System.out.println("cutShot");
+        webServer.shutdown();
+        System.out.println("Webserver shutdown");
+        return true;
+    }
+
+    public boolean pingpong() {
+        sleep(1000);
+        if (new Random().nextInt(10) > 9) {
+            try {
+                cutShot(client);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("PONG");
+            try {
+                pingpong(client);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void pingpong(XmlRpcClient client) throws XmlRpcException, IOException {
+        System.out.println("PING");
+        Vector<Boolean> params = new Vector<Boolean>();
+        client.execute(REMOTE_SERVER_NAME + ".pingpong", params);
+    }
+
+    private static void cutShot(XmlRpcClient client) throws XmlRpcException, IOException {
+        System.out.println("SEND CUT_SHOT");
+        Vector<Boolean> params = new Vector<Boolean>();
+        client.execute(REMOTE_SERVER_NAME + ".cutshot", params);
     }
 
     public Integer echo(int a, int b) {
@@ -21,12 +86,7 @@ public class RpcServer {
 
     public int asy(int sleepTime) {
         System.out.println("Invoked asy: sleepTime=" + sleepTime + "...");
-        try {
-            Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        }
+        sleep(sleepTime);
         System.out.println("... asy - end of sleep");
         return 123;
     }
